bb47295a26434b87375abd238fca57f05bb9a4b5
Artturi Hämäläinen <artturi_hamalainen@hotmail.com>
2019-08-10 18:45:03 +0300

    company page

--- src/components/company.js
+++ src/components/company.js
@@ -1,11 +1,19 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 import Img from "gatsby-image"
 
 const Company = () => {
   const data = useStaticQuery(graphql`
     query {
-      company: file(relativePath: { eq: "trash6.png" }) {
+      company: file(relativePath: { eq: "nico.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      company1: file(relativePath: { eq: "company3.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
@@ -16,34 +24,27 @@ const Company = () => {
   `)
   return (
     <div className="company">
-      <div className="company__left">
-        <h3 className="company__left_title">360 kuvauspalvelu yrityksellesi</h3>
-        <p className="company__left_para">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
-          lacus vel facilisis.
-        </p>
-        <p className="company__left_para">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
-          lacus vel facilisis.
-        </p>
-        <p className="company__left_para">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
-          lacus vel facilisis.
-        </p>
-      </div>
-      <div className="company__right">
-        <div className="company__right_container">
+      <div className="background-container"></div>
+      <div className="company__container">
+        <div className="company__left">
+          <h3 className="company__left_title">Mitäs sitten?</h3>
+          <p className="company__left_para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
+            accumsan lacus vel facilisis. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor
+          </p>
+        </div>
+        <div className="company__right">
           <Img
             fluid={data.company.childImageSharp.fluid}
-            style={{ margin: "1rem", maxHeight: "calc(70vh - 4rem)" }}
+            style={{ margin: "1rem" }}
             imgStyle={{ objectFit: "contain" }}
           ></Img>
+
+          <p>My name is nico, and I am very special boy</p>
+          <p>I like cats and 2d anime grilss</p>
         </div>
       </div>
     </div>
