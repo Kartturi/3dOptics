d5c90882b27dba5c4424db09203f442596b5ec17
Artturi Hämäläinen <artturi_hamalainen@hotmail.com>
2019-08-12 15:06:12 +0300

    asd

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,11 +1,44 @@
 import { useStaticQuery, graphql } from "gatsby"
 import React, { useState } from "react"
+import BackgroundImage from "gatsby-background-image"
 import Overlay from "../components/overlay"
-import Gallery1 from "../components/galleries/gallery1"
-import Gallery2 from "../components/galleries/gallery2"
+import Img from "gatsby-image"
 
 const Gallery = () => {
   const [showProject, setShowProject] = useState(false)
+
+  const data = useStaticQuery(graphql`
+    query {
+      img1: file(relativePath: { eq: "gallery1.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img2: file(relativePath: { eq: "gallery2.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img3: file(relativePath: { eq: "gallery3.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      img4: file(relativePath: { eq: "gallery4.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
   const handleOnClick = () => {
     setShowProject(true)
@@ -22,8 +55,42 @@ const Gallery = () => {
 
       <h3 className="gallery__title">Referenssit</h3>
       <div className="gallery__cases">
-        <Gallery1 callback={handleOnClick}></Gallery1>
-        <Gallery2 callback={handleOnClick}></Gallery2>
+        <div className="gallery__case" onClick={handleOnClick}>
+          <BackgroundImage
+            Tag="div"
+            className="gallery__background_img_1"
+            fluid={data.img1.childImageSharp.fluid}
+            backgroundColor={`#040e18`}
+          ></BackgroundImage>
+          <p className="gallery__case_text">Syrjämän konfrenssikeskus</p>
+        </div>
+        <div className="gallery__case" onClick={handleOnClick}>
+          <BackgroundImage
+            Tag="div"
+            className="gallery__background_img_2"
+            fluid={data.img2.childImageSharp.fluid}
+            backgroundColor={`#040e18`}
+          ></BackgroundImage>
+          <p className="gallery__case_text">Miedon panimo</p>
+        </div>
+        <div className="gallery__case" onClick={handleOnClick}>
+          <BackgroundImage
+            Tag="div"
+            className="gallery__background_img_3"
+            fluid={data.img3.childImageSharp.fluid}
+            backgroundColor={`#040e18`}
+          ></BackgroundImage>
+          <p className="gallery__case_text">Saimaan Konfrenssitilat</p>
+        </div>
+        <div className="gallery__case" onClick={handleOnClick}>
+          <BackgroundImage
+            Tag="div"
+            className="gallery__background_img_4"
+            fluid={data.img4.childImageSharp.fluid}
+            backgroundColor={`#040e18`}
+          ></BackgroundImage>
+          <p className="gallery__case_text">Särmän teknologiakeskus</p>
+        </div>
       </div>
     </div>
   )
